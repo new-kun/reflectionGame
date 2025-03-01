6d399ec7d5ae722f6450658224020490777da02d
Kun <s58803194@gmail.com>
2025-03-01 22:36:33 +0800

    構造函數、屬性與方法

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.extern.log4j.Log4j2;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,8 +22,10 @@ class DemoApplicationTests {
 	}
 
 	@Test
-	@DisplayName("獲取Clss對象")
+	@DisplayName("獲取Clss類別資訊")
 	void excriseClassObj() throws ClassNotFoundException {
+		log.info("使用 Class.forName('類別名稱') 或 ClassName.class 獲取 Class 物件");
+
 		log.info("通過class常量");
 		Class clazz = String.class;
 
@@ -34,17 +38,37 @@ class DemoApplicationTests {
 	}
 
 	@Test
-	@DisplayName("創建對象")
+	@DisplayName("創建實例")
 	void createObj()
       throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+		log.info("通過 Constructir的newInstance()方法創建物件");
+
 		log.info("透過反射動態創建對象");
 		Class clazz = Class.forName("java.lang.String");
 		Object obj = clazz.getDeclaredConstructor().newInstance();
 	}
 
 	@Test
+	@DisplayName("創建實例2")
+	void createInstance(){
+		log.info("通過 Constructir的newInstance()方法創建物件");
+		try {
+			Class<?> clazz = Class.forName("java.lang.String");
+			Constructor<?> constructor = clazz.getConstructor(String.class);
+			Object instance = constructor.newInstance("Hello");
+			System.out.println(instance);
+		} catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException |
+             InstantiationException | IllegalAccessException e) {
+			throw new RuntimeException(e);
+		} finally {
+			log.info("end");
+		}
+
+  }
+
+	@Test
 	@DisplayName("透過反射取得對象的屬性")
-	void getObjFiled() throws NoSuchFieldException, IllegalAccessException {
+	void getObjField() throws NoSuchFieldException, IllegalAccessException {
 		Person persionInstance = new Person("kun",30);
 		Class<Person> clazz = Person.class;
 		//如果屬性是私有的，設置他的訪問權限
@@ -52,22 +76,70 @@ class DemoApplicationTests {
 		field.setAccessible(true);
 		Object value = field.get(persionInstance);//獲取屬性
 		field.set(persionInstance,"New Name");
-		System.out.println(persionInstance);
+	}
+
+	@Test
+	void exerciseGetObjField() throws IllegalAccessException, NoSuchFieldException {
+		Class<?> clazz = Person.class;
+		Field field = clazz.getDeclaredField("age");
+		field.setAccessible(true);
+		Person person = new Person("kun",30);
+		String fieldName = (String) field.getName();
+		System.out.println(fieldName);
+		Object value = field.get(person);
+		System.out.println(value);
+	}
+
+	@Test
+	@DisplayName("調用方法")
+	void getMethods()
+      throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, ClassNotFoundException {
+		Class<?> clazz = Class.forName("java.lang.String");
+		Method method = clazz.getMethod("length");
+		String str = "Hi";
+		Integer length = (Integer) method.invoke(str);
+		System.out.println("字串長度"+ length);
+	}
 
 
+	@Test
+	@DisplayName("獲取構造函數")
+	void exerciseGetConstructor()
+      throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+		Class<?> clazz = Person.class;
+		Constructor<?> constructor = clazz.getDeclaredConstructor(String.class, Integer.class);
+		constructor.setAccessible(true);
+		Object obj = constructor.newInstance("John",42);
+		System.out.println(obj);
+
+	}
+
+	@Test
+	@DisplayName("獲取接口和父類")
+	void exerciseGetInterfaceAndSuper(){
+		log.info("獲取所有接口");
+		Class<?>[]  interfaces = Person.class.getInterfaces();
+
+		for(Class<?> clz : interfaces){
+			System.out.println("Interface: "+clz.getName());
+			System.out.println("-----");
+		}
 	}
 
 
 
-	@Data
-	@AllArgsConstructor
-	public class Person{
-		private String name;
-		private Integer age;
-	}
 
 
 
+
+
+}
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+ class Person{
+	private String name;
+	private Integer age;
 }
 
 
